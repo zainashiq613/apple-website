cf8fde28b5d51312ca3afc6d1dd0aabdf2161bc6
Muhammad Zain <zain.ashiq613@gmail.com>
2025-06-20 23:12:24 +0500

    navbar, hero section, highlights section, modal complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <div>
       <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
         <nav className="flex w-full screen-max-width">
-          <img src="../../public/assets/images/apple.svg" width={14} height={18} />
+          <img src="../../assets/images/apple.svg" width={14} height={18} />
           <div className="flex flex-1 justify-center items-center max-sm:hidden">
             {navLists.map((item, index) => (
               <div
@@ -17,8 +17,8 @@ function Navbar() {
             ))}
           </div>
           <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-            <img src="../../public/assets/images/search.svg" width={18} height={18} alt="" />
-            <img src="../../public/assets/images/bag.svg" width={18} height={18} alt="" />
+            <img src="../../assets/images/search.svg" width={18} height={18} alt="" />
+            <img src="../../assets/images/bag.svg" width={18} height={18} alt="" />
           </div>
         </nav>
       </header>
